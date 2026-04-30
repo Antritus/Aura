e6abd17f4bbb1844a2bb732e162604d14253a3da
Antsu <antt.graft@gmail.com>
2026-04-30 13:05:29 +0300

    Work jitpack dumbass

--- build.gradle.kts
+++ build.gradle.kts
@@ -88,10 +88,11 @@ publishing {
 }
 
 java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
-    }
-    disableAutoTargetJvm()
+	toolchain {
+		languageVersion.set(JavaLanguageVersion.of(21))
+	}
+	sourceCompatibility = JavaVersion.VERSION_17
+	targetCompatibility = JavaVersion.VERSION_17
 }
 
 // Configure plugin.yml generation
